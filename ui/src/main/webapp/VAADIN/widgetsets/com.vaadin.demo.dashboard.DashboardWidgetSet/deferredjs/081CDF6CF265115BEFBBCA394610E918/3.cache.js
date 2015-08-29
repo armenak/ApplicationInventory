560d693720c1efb62f89a9edb9c24c4b464e545b
Armenak Grigoryan <agstrider@gmail.com>
2015-08-29 11:58:24 -0700

    Delete Vaadin project.

--- ui/src/main/webapp/VAADIN/widgetsets/com.vaadin.demo.dashboard.DashboardWidgetSet/deferredjs/081CDF6CF265115BEFBBCA394610E918/3.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_vaadin_demo_dashboard_DashboardWidgetSet.runAsyncCallback3('T9(1,null,{});_.gC=function U(){return this.cZ};eCd(Qh)(3);\n//# sourceURL=com.vaadin.demo.dashboard.DashboardWidgetSet-3.js\n')
